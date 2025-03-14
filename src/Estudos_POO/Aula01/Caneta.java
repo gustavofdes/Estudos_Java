226de483cb12b2a565e86acd1bc9e38474b8d148
gusta <gustavoferreiradoespiritosanto@gmail.com>
2025-03-14 17:54:04 -0300

    Começando estudos de Programação Orientada a Objetos

---
+++ src/Estudos_POO/Aula01/Caneta.java
@@ -0,0 +1,32 @@
+package Estudos_POO.Aula01;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("A caneta é do modelo: " + this.modelo);
+        System.out.println("Da cor: " + this.cor);
+        System.out.println("A porta é: " + this.ponta);
+        System.out.println("A carga dela esta: " + this.carga);
+        System.out.println("Ela esta tampada: " + this.tampada);
+    }
+
+    void tampar(){
+        tampada = true;
+    }
+    void destampar(){
+        this.tampada = false;
+    }
+
+    void rabiscar(){
+        if (!tampada){
+            System.out.println("Estou rabiscando...");
+        } else {
+            System.err.println("ERRO: A caneta esta tampada, impossivel rabiscar");
+        }
+    }
+}
